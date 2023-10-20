3ab5eb6c8ee9f5c922d166dc016022fd52e8f1c1
Kishor S <kishorsbca@gmail.com>
2023-10-20 12:15:26 +0530

    added:first design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightgreen: "#83C5BE",
+      },
+      fontFamily: {
+        poppins: "'Poppins', sans-serif",
+      },
+    },
   },
   plugins: [],
-}
-
+};
